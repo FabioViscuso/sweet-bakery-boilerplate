4c41cf34a73738933dde879736e8f258f8a4c00d
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-07-12 18:36:40 +0200

    feat: added more products and changed a line in homepage

--- src/components/shop/Products.tsx
+++ src/components/shop/Products.tsx
@@ -8,13 +8,16 @@ const DUMMY_PRODUCTS: CartItem[] = [
     { id: 'prod1', imgUrl: 'https://laboratorioespresso.it/wp-content/uploads/2018/12/tiramisu-grande.jpg', title: 'Tiramisù', price: 4.50, description: 'A classic' },
     { id: 'prod2', imgUrl: 'https://i.pinimg.com/originals/ca/ea/08/caea086803a381300f5d5f90b8b74ba7.jpg', title: 'Fruit cupcake', price: 5.00, description: 'Fruity and tasty' },
     { id: 'prod3', imgUrl: 'https://bigoven-res.cloudinary.com/image/upload/t_recipe-1280/ultimatevanillacheesecake.jpg', title: 'Cheesecake', price: 9.99, description: 'With a variety of toppings' },
+    { id: 'prod4', imgUrl: 'https://th.bing.com/th/id/OIP.syJbyeVdvs4kzYLEVV7x9QHaEx?pid=ImgDet&rs=1', title: 'Sacher', price: 12.40, description: 'A triumph of chocolate' },
+    { id: 'prod5', imgUrl: 'https://i.pinimg.com/originals/02/00/cb/0200cb8509c58d95998b0ab929dfe7b8.jpg', title: 'Muffin', price: 6.00, description: 'A breakfast must-have' },
+    { id: 'prod6', imgUrl: 'https://th.bing.com/th/id/R.42fab05ad25810a2d351217dc63a0c95?rik=8INLfDn5jRZ1Vw&riu=http%3a%2f%2fwww.mmcbakes.com%2fwp-content%2fuploads%2f2018%2f12%2fIMG_1790.jpg&ehk=P9%2fzURdG77YdVz14AjThu33iyQ8j1ptkpPPIau%2fSM18%3d&risl=&pid=ImgRaw&r=0', title: 'Macaron', price: 2.00, description: 'For a quick, sweet treat' },
 ]
 
 export const Products = () => {
     return (
         <section className='flex flex-col justify-center items-center w-full'>
             <h2 className='text-4xl mb-8 font-indieflower'>Buy your favorite products</h2>
-            <ul className='flex flex-row flex-wrap gap-5 justify-between items-center w-full px-5'>
+            <ul className='shop-grid gap-5 justify-between items-center w-full px-5'>
                 {
                     DUMMY_PRODUCTS.map(item =>
                         <ProductItem
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -14,7 +14,7 @@ export const Homepage = () => {
             </section>
             <section className="flex flex-row max-w-4xl gap-10 items-center">
                 <div>
-                    <h3 className="font-caveat text-5xl mb-5">As the day they were made</h3>
+                    <h3 className="font-caveat text-5xl mb-5">Freshly made, for you</h3>
                     <p className="font-indieflower text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque labore et praesentium! Assumenda, distinctio sunt fugiat ullam dolorum cum, quidem aspernatur sit fuga facere magnam repudiandae aperiam nisi vitae eos?</p>
                 </div>
                 <img className='w-60 h-60 rounded-full' src="https://th.bing.com/th/id/R.93615fdff3c855317446cd415b7c9ee4?rik=kKX0twgkg%2bkr1g&riu=http%3a%2f%2fpinchables.net%2fwp-content%2fuploads%2f2018%2f01%2fDSC_0765.jpg&ehk=6uewdZVTecQq7eU8ya9ifZpHRAsO2iaOGawrnVMN6ns%3d&risl=&pid=ImgRaw&r=0" alt="homepage-img1" />
