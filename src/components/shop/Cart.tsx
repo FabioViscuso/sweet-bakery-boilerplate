6e28cbab54d35ec382cb4cc5c54eee990f13627a
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-07-17 19:11:26 +0200

    feat: minor visual improvements

--- src/components/shop/Cart.tsx
+++ src/components/shop/Cart.tsx
@@ -14,9 +14,9 @@ export const Cart = () => {
     console.log(items)
 
     return (
-        <div className="absolute -right-2 top-16 flex flex-col justify-between items-center w-screen md:w-max gap-5 py-5 px-2 rounded-md bg-[#e2c3c8] border-stone-600 border-2">
+        <div className="absolute -right-2 top-16 flex flex-col justify-between items-center md:w-max gap-5 py-5 px-2 rounded-md bg-[#e2c3c8] border-stone-600 border-2">
             <h2 className="text-4xl font-caveat">Your Shopping Cart</h2>
-            <ul className="flex flex-col gap-5 w-full">
+            <ul className="flex flex-col gap-5 w-full  max-h-96 overflow-auto">
                 {
                     items.map((item: CartItemWithStats, index: number) =>
                         <CartItemComponent
--- src/components/shop/CartButton.tsx
+++ src/components/shop/CartButton.tsx
@@ -14,10 +14,12 @@ export const CartButton = () => {
 
 
     return (
-        <button className='relative flex flex-row gap-4 items-center font-indieflower text-2xl' onClick={toggleCartHandler}>
-            <span className=''>My Cart</span>
-            <span className='bg-pink-100 text-gray-900 w-10 h-10 rounded-full leading-10'>{totalItems}</span>
+        <div className='relative'>
+            <button className='flex flex-row gap-4 items-center font-indieflower text-2xl' onClick={toggleCartHandler}>
+                <span className=''>My Cart</span>
+                <span className='bg-pink-100 text-gray-900 w-10 h-10 rounded-full leading-10'>{totalItems}</span>
+            </button>
             {showCart && <Cart />}
-        </button>
+        </div>
     );
 };
