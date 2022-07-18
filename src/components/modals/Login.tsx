7ba65b8efe836b79a87cc082d0f8f32958659fb7
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-07-18 23:53:26 +0200

    feat: first draft of login/signup logic

--- src/components/modals/Login.tsx
+++ src/components/modals/Login.tsx
@@ -1,39 +1,47 @@
 // Import hooks
 import { useModals } from "../../utils/useModals"
-import { useLogin } from "../../utils/useLogin";
-
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../store/actions/loginActions";
 
 export const Login = () => {
 
     const { closeLoginHandler } = useModals()
-    const { loginHandler } = useLogin()
+    const dispatch = useDispatch()
+
+    const loginHandler = (event: any) => {
+        event.preventDefault()
+        const username: string = event.target.elements.loginusername.value
+        const password: string = event.target.elements.loginpassword.value
+        console.log(username, password)
+        dispatch(loginUser(username, password))
+    }
 
 
     return (
-        <div onClick={closeLoginHandler} className="py-12 bg-gray-700 bg-opacity-50 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="loginModal">
+        <div className="py-12 bg-gray-700 bg-opacity-50 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="loginModal">
             <div role="alert" className="container mx-auto w-11/12 md:w-2/3 max-w-lg">
-                <form className="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+                <form onSubmit={loginHandler} className="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
                     <h1 className="text-gray-800 text-6xl font-caveat tracking-normal leading-tight mb-4">Log In</h1>
 
                     {/* USERNAME FIELD */}
                     <label htmlFor="loginusername" className="text-gray-800 text-lg font-indieflower leading-tight tracking-normal">Username</label>
-                    <input id="loginusername" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="" />
+                    <input id="loginusername" name="loginusername" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="" />
 
                     {/* PASSWORD FIELD */}
                     <label htmlFor="loginpassword" className="text-gray-800 text-lg font-indieflower leading-tight tracking-normal">Password</label>
                     <div className="relative mb-5 mt-2">
-                        <input type='password' id="loginpassword" className="text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="" />
+                        <input type='password' id="loginpassword" name="loginpassword" className="text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="" />
                     </div>
 
                     {/* BUTTONS */}
                     <div className="flex items-center justify-start w-full">
-                        <button type="submit" onClick={loginHandler} className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 transition duration-150 ease-in-out hover:bg-pink-300 bg-pink-200 rounded text-gray-900 hover:text-gray-900 px-8 py-2 text-md">Submit</button>
+                        <button type="submit" className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 transition duration-150 ease-in-out hover:bg-pink-300 bg-pink-200 rounded text-gray-900 hover:text-gray-900 px-8 py-2 text-md">Submit</button>
                         <button type="button" onClick={closeLoginHandler} className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:bg-gray-300 border rounded px-8 py-2 text-md">Cancel</button>
                     </div>
 
                     {/* CLOSE BUTTON */}
                     <button onClick={closeLoginHandler} className="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" aria-label="close modal">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" />
                             <line x1="18" y1="6" x2="6" y2="18" />
                             <line x1="6" y1="6" x2="18" y2="18" />
---
+++ src/store/actions/loginActions.ts
@@ -0,0 +1,83 @@
+import { ThunkDispatch } from "redux-thunk"
+import { ThunkAction, AnyAction } from "@reduxjs/toolkit"
+import { uiSliceActions } from "../slices/uiSlice"
+import { loginActions } from "../slices/loginSlice"
+
+
+export const signupNewUser = (username: string, password: string): any/* ThunkAction<Promise<void>, {}, {}, AnyAction> */ => {
+    return async (dispatch: ThunkDispatch<{}, {}, AnyAction>) => {
+        const fetchDB = async () => {
+            const response = await fetch('https://react-cart-demo-f429b-default-rtdb.europe-west1.firebasedatabase.app/users.json')
+
+            if (!response.ok) {
+                throw new Error('Fetching data failed')
+            }
+
+            const data = await response.json() || []
+            console.log('userdata', ...data)
+
+            if (data.some((item: { username: string }) => item.username === username)) {
+                dispatch(uiSliceActions.showNotification({
+                    status: 'error',
+                    title: `User ${data.username} already exists`,
+                    message: 'Please enter a different username'
+                }))
+                throw new Error('Existing user')
+            } else {
+                const usersToUpload: any = [...data, { username: username, password: password }]
+
+                fetch('https://react-cart-demo-f429b-default-rtdb.europe-west1.firebasedatabase.app/users.json', {
+                    method: 'PUT',
+                    body: JSON.stringify(usersToUpload),
+                })
+            }
+        }
+        try {
+            await fetchDB()
+
+        } catch (err: any) {
+            // otherwise the notification will send a negative message
+            dispatch(uiSliceActions.showNotification({
+                status: 'error',
+                title: 'Data sending failed',
+                message: err.message
+            }))
+            console.log('failed signup', err)
+        }
+    }
+}
+
+export const loginUser = (username: string, password: string): any /* ThunkAction<Promise<void>, {}, {}, AnyAction> */ => {
+    return async (dispatch: ThunkDispatch<{}, {}, AnyAction>) => {
+        const fetchDB = async () => {
+            const response = await fetch('https://react-cart-demo-f429b-default-rtdb.europe-west1.firebasedatabase.app/users.json')
+
+            if (!response.ok) {
+                throw new Error('Fetching data failed')
+            }
+
+            const data = await response.json()
+
+            if (!data.some((item: { username: string, password: string }) => (item.username === username && item.password === password))) {
+                dispatch(uiSliceActions.showNotification({
+                    status: 'error',
+                    title: `Invalid credentials`,
+                    message: 'Please check again'
+                }))
+                throw new Error('Invalid credentials')
+            } else {
+                dispatch(loginActions.login())
+            }
+        }
+        try {
+            await fetchDB()
+        } catch (err: any) {
+            // otherwise the notification will send a negative message
+            dispatch(uiSliceActions.showNotification({
+                status: 'error',
+                title: 'Data retrieve failed',
+                message: err.message
+            }))
+        }
+    }
+}
